982af6bdd26523039a90c6c2ee5a1ca7cbaa229c
chandru-006 <88603801+chandru-006@users.noreply.github.com>
2021-08-08 08:26:53 +0530

    Add files via upload

---
+++ LearnDataTypeAndVariables.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class LearnDataTypeAndVariables {
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		String brandname = "Poco";
+		String model = "X3pro";
+		int price = 19999;
+		byte memory = 64;
+		float screensize = 6.5f;
+		boolean quickcharge = true;
+		System.out.println("This is " + brandname + " with the price of " + price + " ROM SIZE " + memory
+				+ " and bigger display size of " + screensize + " And does it support fast charge " + quickcharge);
+	}
+}
---
+++ Work3.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Work3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 51; i++) {
+			
+			if (i%2 == 0) {
+				System.out.println("even");
+			}
+			
+			else
+			{
+				System.out.println("ODD");
+			}
+		}
+	}
+
+}
