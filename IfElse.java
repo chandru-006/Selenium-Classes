11bfce2decc6298049eaa73b81b60f3c5cbdceec
chandru-006 <88603801+chandru-006@users.noreply.github.com>
2021-08-08 08:35:54 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 101;
+		if (i%2 == 0 ) {
+			System.out.println("the number is even");
+
+		} else {
+			System.out.println("the number is odd");
+		}
+
+	}
+
+}
