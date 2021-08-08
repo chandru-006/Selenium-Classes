5cd56fe749882edc7838cfa30296f2102b193cc0
chandru-006 <88603801+chandru-006@users.noreply.github.com>
2021-08-08 09:38:40 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 101; i++)
+			if (i % 2 == 0) {
+				System.out.println(i+" - even");
+
+			} else {
+				System.out.println(i+" -odd");
+
+			}
+	}
+
+}
